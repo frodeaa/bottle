8781c877e0af926e238c25e309df5bd3e6fb6b9d
Frode Aannevik <frode.aannevik@dsafe.no>
2015-12-25 19:46:13 +0100

    implement login and list of bottles

--- src/main/chrome-extension/popup.js
+++ src/main/chrome-extension/popup.js
@@ -1,7 +1,9 @@
 (function(){
     'use strict';
     var serverURI = 'http://localhost:9000',
-        config = null;
+        config = null,
+        count = 0,
+        links = document.getElementById("links");
 
     var doXhr = function(method, path, auth, data, callback) {
         var req = new window.XMLHttpRequest();
@@ -24,19 +26,81 @@
         };
     };
 
+    var newUser = function(callback) {
+        console.log("create new user");
+        var userData = {password: Math.random().toString(36)};
+        doXhr("POST", "/users", null, userData, function(err, data) {
+            if (!err) {
+                data.password = userData.password;
+                callback(null, config);
+            } else {
+                callback(err);
+            }
+        });
+    };
+
+    var badgeText = function(c){
+        if(c > 999){
+            return c.toString()+"+";
+        }
+        return c.toString();
+    };
+
+    var getIcon = function(url) {
+        var domain = url.replace('http://','').replace('https://','').split(/[/?#]/)[0];
+        var imgUrl = "http://www.google.com/s2/favicons?domain=" + domain;
+        var img = document.createElement("img");
+        img.setAttribute('src', imgUrl);
+        return img.outerHTML;
+    };
+
+    var createLinkHtml = function(bottle) {
+         var linkBtn = document.createElement("span");
+         linkBtn.setAttribute("class", "removeBtn");
+         linkBtn.setAttribute("id", bottle.id);
+         return linkBtn.outerHTML+"<a target='_blank' href='"+bottle.url+"'>" + getIcon(bottle.url) + " " + bottle.title +"</a>";
+    };
+
+    var listBottles = function(err, bottles) {
+        if (err) {
+            count = 0;
+            return;
+        }
+        count = bottles.length;
+        for (var i = 0; i < count; i++) {
+            var list = document.createElement("li");
+            list.innerHTML= createLinkHtml(bottles[i]);
+            links.appendChild(list);
+        }
+        chrome.browserAction.setBadgeText({"text": badgeText(count)});
+        console.log("finish list bottles", count);
+    };
+
+    var getBottles = function(callback) {
+        callback(null, []);
+    };
+
+
+
     chrome.storage.sync.get("bottle-cfg", function(cfg) {
         config = cfg;
         if ('bottle-cfg' in config) {
             config = config['bottle-cfg'];
         }
         if (!("password" in config) || !('id' in config)) {
-            console.log("no config found, login to get new");
+            console.log("no config found");
+            newUser(function(err, data) {
+                if (!err) {
+                    config = data;
+                    chrome.storage.sync.set( {'bottle-cfg': config});
+                    listBottles(null, []);
+                } else {
+                    console.log("failed to create new user", err);
+                }
+            });
         } else {
-            console.log("found counfig, continue");
+            console.log("use existing config");
+            getBottles(listBottles);
         }
     });
-
-    doXhr("GET", "/bottle", "", null, null);
-
-
 })();
